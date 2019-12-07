4db5f77c3977b0eefd389ff3dab134a9123977c4
Terenia <teqera@outlook.com>
2019-12-07 11:28:37 +0100

    Pierwszy commmit do projektu

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Testowanie zamieszczania plików do gita
+ *
+ * @author teresa.salwocka@o2.pl
+ * @since 07.12.2019
+ **/
+final class Main {
+    public static void main(String[] args) {
+
+    }
+
+}
